81b4383b3abec4683b56034ef81911838b66eadc
PawelMolek <pawel.molek@gmail.com>
2022-02-28 17:53:56 +0100

    Script.rb updated after comments

--- script.rb
+++ script.rb
@@ -1,18 +1,17 @@
 class User
-  attr_accessor :name,  :surname, :email, :phone_number
+  attr_accessor :name, :surname, :email, :phone_number
 
-  def initialize (name,surname,email,phone_number)
+  def initialize(name, surname, email, phone_number)
     @name = name
     @surname = surname
     @email = email
     @phone_number = phone_number
   end
-
 end
 
-pawel = User.new("Pawel", "Pawlowski", "pawel@pawel.pl", "123456789")
-stefan = User.new("Stefan", "Stefanowski", "stefan@stefan.pl", "987654321")
-gerwazy = User.new("Gerwazy", "Gerwazowski", "gerwazy@gerwazy.pl", "111222333")
+user1 = User.new("Pawel", "Pawlowski", "pawel@pawel.pl", 123456789)
+user2 = User.new("Stefan", "Stefanowski", "stefan@stefan.pl", 987654321)
+user3 = User.new("Gerwazy", "Gerwazowski", "gerwazy@gerwazy.pl", 111222333)
 
 class Admin < User
   def welcome
@@ -20,14 +19,13 @@ class Admin < User
   end
 end
 
-roman = Admin.new("Roman", "Romanowski", "roman@roman.pl", "444555666" )
-andrzej = Admin.new("Andrzej", "Andrzejowski", "andrzej@andrzej.pl", "777888999" )
+user4 = Admin.new("Roman", "Romanowski", "roman@roman.pl", 444555666)
+user5 = Admin.new("Andrzej", "Andrzejowski", "andrzej@andrzej.pl", 777888999)
 
-array = []
-array<<pawel<<stefan<<gerwazy<<roman<<andrzej
+array = [user1, user2, user3, user4, user5]
 
 array.each do |user|
-  if (user.phone_number.to_i.digits.sum).even?
+  if (user.phone_number.digits.sum).even?
     puts user.phone_number
   end
 end
